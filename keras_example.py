990195fa651b19ea499fe9ed33a664bfb996e396
Joshua Sizer <joshsizer@gmail.com>
2021-04-28 12:19:16 -0400

    Added credits and some useful comments

--- keras_example.py
+++ keras_example.py
@@ -1,6 +1,10 @@
+# Thanks to:
+# https://elitedatascience.com/keras-tutorial-deep-learning-in-python
+# for the guide.
+
 import numpy as np
-# initial seed
-# np.random.seed(123)
+# initial seed for reproducibility
+np.random.seed(3411)
 
 from tensorflow import keras
 from keras.models import Sequential
@@ -9,23 +13,42 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.datasets import mnist
 
+# Load the training and test sets
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 from matplotlib import pyplot as plt
+
+# Uncomment to see a visual representation of
+# the input data
 plt.imshow(X_train[1])
 
+# We have to include the depth of the image.
+# The depth is the number of channels. HSV or RGB
+# have three channels. These images have only one
+# channel (black and white).
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
+# This is a rather manual way to normalize the
+# data. But, we want values between 0 and 1, not 0
+# and 255.
 X_train /= 255
 X_test /= 255
 
+# Normally the training data (expected outputs) is
+# just a single number per input object. However,
+# to compare the expected vs calculated values in
+# back-propagation, you need to have the Y values
+# be an 10 length array. 
+# ie: 4 -> [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
 Y_train = np_utils.to_categorical(Y_train, 10)
 Y_test = np_utils.to_categorical(Y_test, 10)
 
+# Are we training a model from file, or creating 
+# a new one?
 model = None
 current_model_path = "models/model2"
 
@@ -34,7 +57,13 @@ try:
 except IOError:
     model = Sequential()
 
+    # Set the model architecture:
+    # softmax in the last layer + 
+    # cross-entropy loss function is good for 
+    # categorization
     model.add(Convolution2D(32, 3, 3, activation="relu", input_shape=(28, 28, 1), data_format="channels_last"))
+    # uncomment the next line to make the
+    # performance worse 
     # model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.25))
     model.add(Flatten(input_shape=(28, 28, 1)))
@@ -48,9 +77,13 @@ except IOError:
                 optimizer="adam",
                 metrics=['accuracy'])
 
+# Do the learning!
+# The train data is split into 70% for training
+# and 30% for validation.
 model.fit(X_train, Y_train, batch_size=32, epochs=100, validation_split=0.3,  verbose=1)
 
 model.save(current_model_path)
 
+# How did we do?
 score = model.evaluate(X_test, Y_test, verbose=0)
 print(f'Model sore: {score}')
--- pytorch_example.py
+++ pytorch_example.py
@@ -1,17 +1,24 @@
+# Thanks to:
+# https://ashleyy-czumak.medium.com/mnist-digit-classification-in-pytorch-302476b34e4f
+# for the guide.
+
 import torch as t
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+# The transformation to apply to the dataset
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,)),])
 
+# Either download the datasets, or load them from
+# file
 mnist_trainset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
 train_loader = t.utils.data.DataLoader(mnist_trainset, batch_size=32, shuffle=True)
 mnist_testset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
 test_loader = t.utils.data.DataLoader(mnist_testset, batch_size=32, shuffle=True)
 
-
+# Here is our network architecture
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
@@ -27,12 +34,16 @@ class Network(nn.Module):
         x = self.final(x)
         return x
 
+# Create a network
 net = Network()
 
+# Define our loss function as well as gradient
+# decent algorithm.
 cross_el = nn.CrossEntropyLoss()
 optimizer = t.optim.Adam(net.parameters(), lr=0.01)
 epoch = 10
 
+# Do the learning!
 for epoch in range(epoch):
     net.train()
 
@@ -47,6 +58,7 @@ for epoch in range(epoch):
 correct = 0
 total = 0
 
+# How well did we do?
 with t.no_grad():
     for data in test_loader:
         x, y = data
